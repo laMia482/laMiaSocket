0cb348650c26d903d5578911110736364cd3f4c3
e0024 <wangqiang482@icloud.com>
2017-07-21 20:35:03 +0800

    add build.sh to build program, modify sockettype in client.cpp to let it be client or server

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ client.cpp laMiaSocket.h -o client -DLinux `pkg-config --libs opencv` `pkg-config --cflags opencv` 
--- client.cpp
+++ client.cpp
@@ -1,18 +1,25 @@
 #include "laMiaSocket.h"
+#include <opencv2/opencv.hpp>
 
 int main(int argc, char **argv)
 {
 
 #if 1
-	laMiaSocket *ls = new laMiaSocket(laMiaSocketTypeClient, 4999);
-	
-	// ls->listenClient();
-	ls->connectServer();
-	
+	laMiaSocket *ls = new laMiaSocket(laMiaSocketTypeServer, 4999);
+	// ls->setBufSize(25*25);
+	ls->bindSocket();
+	ls->listenClient();
+	// ls->connectServer();
+	// cv::Mat image = cv::imread("image.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	// cv::resize(image, image, cv::Size(25,25));
 	while (true)
 	{
-		ls->sendMessage();
+		// getchar();
+		// ls->sendMessage((char*)image.data);
+		// ls->sendMessage();
 		ls->recvMessage();
+		logStr(ls->m_pRecvMessage);
+		system(ls->m_pRecvMessage);
 	}
 
 #endif
